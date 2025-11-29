f64ffa68eab6213923ae7ba37e5972660ba8ffa5
Sidny8 <telc090311mgtnpna1@cbtis217.edu.mx>
2025-11-29 15:24:32 -0600

    Ejercicio C completo por individual

--- TrabajoEnEquipo/TrabajoEnEquipo/Datos.cs
+++ TrabajoEnEquipo/TrabajoEnEquipo/Datos.cs
@@ -17,6 +17,9 @@ namespace TrabajoEnEquipo
 	/// </summary>
 	public partial class Datos : Form
 	{
+		public string Nombre;
+		public string Semestre;
+		public string Especialidad;
 		public Datos()
 		{
 			//
@@ -29,19 +32,40 @@ namespace TrabajoEnEquipo
 			//
 		}
 		
-		void TextBox4TextChanged(object sender, EventArgs e)
-		{
-			
-		}
 		
-		void Label10Click(object sender, EventArgs e)
-		{
-			
-		}
 		
-		void TextBox6TextChanged(object sender, EventArgs e)
+		void BtmRegistrarClick(object sender, EventArgs e)
 		{
+			if(cboEspecialidad.SelectedIndex== -1)
+				MessageBox.Show("Debera de escojer una opcion de las dichas especialidades ||");
+			else if(cboSemestre.SelectedIndex==-1)
+				MessageBox.Show("Deberia de escojer una opcion ya encionada en semestre ||");
+			else if(cboColonia.SelectedIndex==-1)
+				MessageBox.Show("Deberia de escojer una Colonia||");
+			else if(txtName.Text=="")
+				MessageBox.Show("Diga el nombre||");
+			else if(txtApellido.Text=="")
+				MessageBox.Show("Oblgatorio su apellido ||");
+			else if(txtCorreo.Text=="")
+				MessageBox.Show("Obligatorio su correo ||");
+			else if(txtTelefono.Text=="0" || txtTelefono.Text=="")
+				MessageBox.Show("Obligatorio su telefono ||");
+			else if(txtDomicilia.Text=="0" || txtTelefono.Text=="")
+				MessageBox.Show("Diga su dirreccion ||");
+			else{
+				MessageBox.Show("Guardado exitosamente ||");
+				txtAltura.Text="";
+				txtApellido.Text="";
+				txtCorreo.Text="";
+				txtDomicilia.Text="";
+				txtName.Text="";
+				txtPeso.Text="";
+				txtTelefono.Text="";
+				cboColonia.SelectedIndex=-1;
+				cboEspecialidad.SelectedIndex=-1;
+				cboSemestre.SelectedIndex=-1;
 			
+			}
 		}
 	}
 }
--- TrabajoEnEquipo/TrabajoEnEquipo/Program.cs
+++ TrabajoEnEquipo/TrabajoEnEquipo/Program.cs
@@ -24,7 +24,7 @@ namespace TrabajoEnEquipo
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Datos());
+			Application.Run(new MainForm());
 		}
 		
 	}
