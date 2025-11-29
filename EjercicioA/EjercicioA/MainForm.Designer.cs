db6a25e1ff6e16f6d520bf9c31fc4b9f2af45139
Sam1234xd <gucs090629mgtznma8@cbtis217.edu.mx>
2025-11-28 19:52:09 -0600

    Ya mero

---
+++ EjercicioA/EjercicioA/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC39
+ * Date: 28/11/2025
+ * Time: 07:03 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace EjercicioA
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtUser = new System.Windows.Forms.TextBox();
+			this.txtPass = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Snap ITC", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(129, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "LOGIN";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(61, 56);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Usuario:";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(51, 82);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(81, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Contraseña:";
+			// 
+			// txtUser
+			// 
+			this.txtUser.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUser.Location = new System.Drawing.Point(138, 56);
+			this.txtUser.Name = "txtUser";
+			this.txtUser.Size = new System.Drawing.Size(176, 23);
+			this.txtUser.TabIndex = 3;
+			// 
+			// txtPass
+			// 
+			this.txtPass.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtPass.Location = new System.Drawing.Point(138, 82);
+			this.txtPass.Name = "txtPass";
+			this.txtPass.Size = new System.Drawing.Size(176, 23);
+			this.txtPass.TabIndex = 4;
+			this.txtPass.Text = "CONTRASEÑA";
+			this.txtPass.Enter += new System.EventHandler(this.TxtPassEnter);
+			this.txtPass.Leave += new System.EventHandler(this.TxtPassLeave);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button1.Font = new System.Drawing.Font("Imprint MT Shadow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(77, 134);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(237, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "ACCEDER";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Pink;
+			this.ClientSize = new System.Drawing.Size(385, 216);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.txtPass);
+			this.Controls.Add(this.txtUser);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "EjercicioA";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtPass;
+		private System.Windows.Forms.TextBox txtUser;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ EjercicioA/EjercicioA/MainForm.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC39
+ * Date: 28/11/2025
+ * Time: 07:03 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace EjercicioA
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			 if(txtUser.Text == "USUARIO")
+				{
+				  txtUser.Text ="";
+				  txtUser.ForeColor=Color.Black;
+				}
+            }
+			void TxtUserLeave(object sender, EventArgs e)
+		   {
+			//Cunado el cursor sale de la caja de texto
+			if(txtUser.Text == "USUARIO")
+				{
+					txtUser.Text ="";
+					txtUser.ForeColor=Color.DimGray;
+				}
+			}
+			void TxtPassEnter(object sender, EventArgs e)
+			{
+			//Cunado el cursor esta dentro de la caja de texto
+			if(txtPass.Text == "CONTRASEÑA")
+				{
+					txtPass.Text ="";
+					txtPass.ForeColor=Color.Black;
+					txtPass.UseSystemPasswordChar=true;
+				}
+			}
+			void TxtPassLeave(object sender, EventArgs e)
+			{
+			//Cunado el cursor sale de la caja de texto
+			if(txtPass.Text == "CONTRASEÑA")
+				{
+					txtPass.Text ="";
+					txtPass.ForeColor=Color.DimGray;
+					txtPass.UseSystemPasswordChar=false;
+				}
+			}
+		}
+	}
