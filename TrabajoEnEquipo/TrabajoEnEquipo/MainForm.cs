baf52c48eb56769b7679d49f4f03a85a38e1c14d
Sidny8 <telc090311mgtnpna1@cbtis217.edu.mx>
2025-11-28 18:49:27 -0600

    A trabar

---
+++ TrabajoEnEquipo/TrabajoEnEquipo/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC40
+ * Date: 28/11/2025
+ * Time: 06:35 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TrabajoEnEquipo
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
