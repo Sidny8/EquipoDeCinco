d9c10d0aea4a642de7f494bac8583edb79d008ae
Sidny8 <telc090311mgtnpna1@cbtis217.edu.mx>
2025-11-30 16:00:45 -0600

    Contraseña y usuario terminado, individual

--- TrabajoEnEquipo/TrabajoEnEquipo/MainForm.cs
+++ TrabajoEnEquipo/TrabajoEnEquipo/MainForm.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace TrabajoEnEquipo
 {
@@ -28,6 +29,41 @@ namespace TrabajoEnEquipo
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+		}
+		
+		void TxtPassEnter(object sender, EventArgs e)
+		{
+			if(txtPass.Text == "")
+				{
+					txtPass.Text ="";
+					txtPass.ForeColor=Color.Black;
+					txtPass.UseSystemPasswordChar=true;
+				}
+		}
+		
+		void TxtPassLeave(object sender, EventArgs e)
+		{
+			if(txtPass.Text == "")
+				{
+					txtPass.Text ="";
+					txtPass.ForeColor=Color.DimGray;
+					txtPass.UseSystemPasswordChar=false;
+				}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(txtUser.Text==""){
+				MessageBox.Show("Agregue nombre de usuario");
+			}
+			else if(txtPass.Text==""){
+				MessageBox.Show("Escriba la contraseña");
+			}
+			else if(txtUser.Text=="Admin" && txtPass.Text=="123"){
+				MessageBox.Show("Finjamos que cambiamos de pestaña");
+			}else{
+				MessageBox.Show("Contraseña o usuario incorrectas");
+			}
 		}
 	}
 }
